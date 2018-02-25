fcc0b3d0d207124a0d3aa09f31a38aac9be93248
Ingon Ngamnoi <iiengon@Ingons-MacBook-Air.local>
2018-02-25 21:17:00 +0700

    Basic NodeJs Express GET POST

--- express.js
+++ express.js
@@ -1,6 +1,35 @@
 var express=require('express');
-var routing=express();
-routing.get('/',function(req,res){
+var body=require('body-parser');
+var app=express();
+app.use(body());
+/*routing.get('/',function(req,res){
     res.send("Hello world");
-})
-routing.listen(3000);
+});
+routing.get('/profile/:name',function(req,res){ //send value parameter
+    res.send("Node JS"+req.params.name+".");
+});*/
+
+app.set('view engine','ejs'); //set view
+app.get('/',function(req,res){
+    res.render('home');
+});
+app.get('/about',function(req,res){
+    res.render('about');
+});
+app.get('/menu',function(req,res){
+    res.render('menu');
+});
+/* get login.ejs*/
+app.get('/login1',function(req,res){
+    res.render("login1.ejs");
+});
+app.post('/showUser',function(req,res){
+    data={
+        email:req.body.email,
+        password:req.body.password
+    }
+    console.log(data);
+    res.end(JSON.stringify(data));
+});
+//end get
+app.listen(3000);
